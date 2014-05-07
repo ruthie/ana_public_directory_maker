6befdbf051f19112d24ae748c20a5960fb7632e0
Ruthie Byers <ruthie@mit.edu>
2014-05-07 13:29:56 -0700

    A script for turning the CSV of the ANA directory into a public directory html fragment

---
+++ directory_maker.rb
@@ -0,0 +1,58 @@
+require "csv"
+require "erb"
+require_relative "./state_names"
+
+states = {}
+canada = []
+other = []
+
+def email_redact(email)
+  unless email
+    return nil
+  end
+  email.downcase.
+    sub("hyperlink:", "").
+    sub("mailto:", "").
+    sub("@", "(at)").
+    gsub(".", "(dot)")
+    #TODO:  this doesn't deal gracefully with people with dots in their usernames
+    #TODO:  make all emails downcase
+end
+
+CSV.foreach("/home/ruthie/Desktop/ANA/players-4-30-14.csv") do |parts|
+  #opt in for directory
+  if !["yes", "Y", "y", "on"].include?(parts[14])
+    next
+  end
+
+  player = {}
+
+  #list nyckelharpa.org email if it exists, otherwise other email
+  state = parts[9]
+  player['email'] = email_redact(parts[1]) || email_redact(parts[0])
+  player['first_name'] = parts[4].strip
+  player['last_name'] = parts[5].strip
+
+  if state != nil
+    state.strip!
+
+    state_name = $state_abbr[state]
+    if state_name
+      states[state_name] = [] unless states[state_name]
+      states[state_name] << player     
+    elsif province_name = $province_abbr[state]
+      player['location'] = province_name
+      canada << player
+    else
+      player['location'] = state
+      other << player      
+    end
+  end
+end
+
+template_filename="/home/ruthie/Desktop/ANA/directory.html.erb"
+template = ERB.new(File.read(template_filename))
+
+output_filename="/home/ruthie/Desktop/ANA/new_directory.html"
+output_file = File.open(output_filename, 'w')
+output_file.write(template.result)
